659a1a3ca2f216c32a366b4d48a092ae70f40ede
Tanbir Haque <tanbirhaque53@gmail.com>
2024-04-26 03:05:15 +0600

    final commit

--- src/Layout/Pages/ShopPage/ShopPage.jsx
+++ src/Layout/Pages/ShopPage/ShopPage.jsx
@@ -2,7 +2,7 @@ import ProductCard from "../../Components/Shared/ProductCard/ProductCard";
 
 const ShopPage = () => {
     return (
-        <div>
+        <div className="max-w-[1296px] mx-auto">
             <div>
                 <div><p className="text-[44px] font-medium">Shop Grid</p></div>
                 <div className="text-sm breadcrumbs">
@@ -13,10 +13,12 @@ const ShopPage = () => {
                 </div>
             </div>
 
-            <div>
-                <div className="max-w-[330px]">
+            <div className="grid grid-cols-4">
+                {/* Side bar */}
+                <div className="max-w-[330px] flex flex-col gap-10">
                     {/* Price Range */}
                     <div>
+                        <p className="font-medium text-[18px] mb-3">Price Filter</p>
                         <input type="range" min={0} max="100" value="40" className="range range-success" />
                         <div className="flex justify-between items-center">
                             <span className="text-[#55585b] text-[14px]">$0 - $79</span>
@@ -114,9 +116,56 @@ const ShopPage = () => {
                             </div>
                         </div>
                     </div>
+                    {/* Popular brands */}
+                    <div>
+                        <div className="mb-7">
+                            <p className="font-medium text-[18px]">Popular brands</p>
+                            <div className="divider m-0 h-0"></div>
+                        </div>
+                        <div className="grid grid-cols-2 gap-8">
+                            <img src="https://i.ibb.co/Ydf3C1H/brand-1.png" alt="" />
+                            <img src="https://i.ibb.co/GTzWqnP/brand-2.png" alt="" />
+                            <img src="https://i.ibb.co/YPzHpCD/brand-3.png" alt="" />
+                            <img src="https://i.ibb.co/n3Wktzh/brand-4.png" alt="" />
+                            <img src="https://i.ibb.co/xqTCvsL/brand-5.png" alt="" />
+                            <img src="https://i.ibb.co/SfBRy61/brand-6.png" alt="" />
+                        </div>
+                    </div>
+                    {/* Reset filter */}
+                    <div>
+                        <div className="mb-7">
+                            <p className="font-medium text-[18px]">Popular brands</p>
+                            <div className="divider m-0 h-0"></div>
+                        </div>
+                        <button className="bg-[#678e61] px-[48px] py-[9px] text-white rounded-full">Reset Filter</button>
+                    </div>
                 </div>
-                <div>
-                    <ProductCard></ProductCard>
+                <div className="mt-12 col-span-3 ml-8">
+                    <div className="mb-7">
+                        <div className="flex gap-2 items-center">
+                            <button className="border border-black p-2">
+                                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16.3327 6.01341V2.98675C16.3327 2.04675 15.906 1.66675 14.846 1.66675H12.1527C11.0927 1.66675 10.666 2.04675 10.666 2.98675V6.00675C10.666 6.95341 11.0927 7.32675 12.1527 7.32675H14.846C15.906 7.33341 16.3327 6.95341 16.3327 6.01341Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16.3327 15.18V12.4867C16.3327 11.4267 15.906 11 14.846 11H12.1527C11.0927 11 10.666 11.4267 10.666 12.4867V15.18C10.666 16.24 11.0927 16.6667 12.1527 16.6667H14.846C15.906 16.6667 16.3327 16.24 16.3327 15.18Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M7.33268 6.01341V2.98675C7.33268 2.04675 6.90602 1.66675 5.84602 1.66675H3.15268C2.09268 1.66675 1.66602 2.04675 1.66602 2.98675V6.00675C1.66602 6.95341 2.09268 7.32675 3.15268 7.32675H5.84602C6.90602 7.33341 7.33268 6.95341 7.33268 6.01341Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path><path d="M7.33268 15.18V12.4867C7.33268 11.4267 6.90602 11 5.84602 11H3.15268C2.09268 11 1.66602 11.4267 1.66602 12.4867V15.18C1.66602 16.24 2.09268 16.6667 3.15268 16.6667H5.84602C6.90602 16.6667 7.33268 16.24 7.33268 15.18Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                            </button>
+                            <button className="border h-[40px] w-[40px] flex items-center justify-center">
+                                <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 7.11108H1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M15 1H1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M15 13.2222H1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                            </button>
+                            <p>Showing 1-9 of 17 results</p>
+                        </div>
+                        {/* Dropdown */}
+                        <div>
+
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 gap-5">
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                        <ProductCard></ProductCard>
+                    </div>
                 </div>
             </div>
         </div>
